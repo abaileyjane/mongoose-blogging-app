c7e517f2d635b150c06095d10e0cf873c7892eaa
Alanna Bailey <abaileyjane@gmail.com>
2018-01-08 10:39:53 -0500

    prelim get request

--- server.js
+++ server.js
@@ -13,3 +13,19 @@ const {Post}=require('./models');
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
+
+app.get('/posts', (req,res)=>{
+	Post
+		.find()
+		.limit(10)
+		.then(posts =>{
+			res.json({
+				posts: posts.map(
+					(post)=>post.serialize())
+			});
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message:'Internal Server Error'});
+		});
+});
